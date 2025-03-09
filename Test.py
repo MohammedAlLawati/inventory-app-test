c6ddaf1337c450634f5603336abbe50305a1b23c
Mohammed Al Lawati <106488784+MohammedAlLawati@users.noreply.github.com>
2025-03-09 06:26:00 +0000

    adding some database components

--- Test.py
+++ Test.py
@@ -40,3 +40,52 @@ selected_option = st.sidebar.selectbox("Choose an option", options)
 
 st.write("You selected: ", selected_option)
 
+
+
+
+def connect_db():
+    """Connects files to the SQlite Database"""
+    DB_FILENAME = Path(__file__).parent / "inventory.db"
+    db_already_exists = DB_FILENAME.exists()
+
+    conn = sqlite3.connect(DB_FILENAME)
+    db_was_just_created = not db_already_exists
+
+    return conn, db_was_just_created
+
+def initialize_data(conn):
+    """initializes initial inventory data, if it has not been created before"""
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS inventory (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            item_name TEXT,
+            price REAL,
+            units_sold INTEGER,
+            units_left INTEGER,
+            cost_price REAL,
+            reorder_point INTEGER,
+            description TEXT,
+        )
+        """
+    )
+
+
+    cursor.execute(
+        """
+        INSERT INTO inventory
+            (item_name, price, units_sold, units_left, cost_price, reorder_point, description)
+        VALUES
+            ('PV Panels (Jinko)', 1.50, 115, 15, 0.80, 16, 'Hydrating bottled water'),
+            ('Soda (355ml)', 2.00, 93, 8, 1.20, 10, 'Carbonated soft drink'),
+            ('Energy Drink (250ml)', 2.50, 12, 18, 1.50, 8, 'High-caffeine energy drink')
+        """
+    )
+    conn.commit()
+
+    
+
+
+
